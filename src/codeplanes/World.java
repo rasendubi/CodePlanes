d566830332c265b532f93bd0a81d8a9e9f71a5e1
Oleksandr <sasha.sochka@gmail.com>
2013-03-02 23:59:02 +0200

    [World] Add hashCode method

--- src/codeplanes/World.java
+++ src/codeplanes/World.java
@@ -2,18 +2,25 @@ package codeplanes;
 
 public class World {
     public final int tick;
-    
+
     World(int tick) {
         this.tick = tick;
     }
-    
+
+    @Override
+    public int hashCode() {
+        return tick;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof World)) {
             return false;
-        } else {     
+        } else {
             World that = (World)obj;
             return this.tick == that.tick;
         }
+
     }
+
 }
--- test/codeplanes/WorldTest.java
+++ test/codeplanes/WorldTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+
 public class WorldTest {
 
     @Test
@@ -11,10 +13,17 @@ public class WorldTest {
         World w1 = new World(1);
         World w2 = new World(1);
         World w3 = new World(2);
-        
+
+        assertFalse(w1.equals(null));
         assertTrue(w1.equals(w2));
         assertFalse(w1.equals(w3));
         assertFalse(w1.equals("hello"));
+
+        HashSet<World> worldSet = new HashSet<>();
+        worldSet.add(w1);
+        assertTrue(worldSet.contains(w1));
+        assertTrue(worldSet.contains(w2));
+        assertFalse(worldSet.contains(w3));
     }
 
 }
