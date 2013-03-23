2c4dcae9c4addae9830054aea260066790d66502
Oleksandr <sasha.sochka@gmail.com>
2013-03-23 20:43:32 +0200

    [Bullet] Add bullet class

---
+++ test/codeplanes/BulletTest.java
@@ -0,0 +1,39 @@
+package codeplanes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.awt.Point;
+import java.util.HashSet;
+
+public class BulletTest {
+    final private Bullet bullet1 = new Bullet(0, new Point(1,1), 3.1, 3.3, 0);
+    final private Bullet bullet2 = new Bullet(0, new Point(1,1), 3.1, 3.3, 0);
+    final private Bullet bullet3 = new Bullet(3, new Point(3,3), 2.2, 9.9, 4);
+
+    @Test
+    public void testEquals() {
+        assertEquals(bullet1, bullet2);
+        assertFalse(bullet1.equals(bullet3));
+    }
+
+    @Test
+    public void testHashCode() {
+
+        assertEquals(bullet1.hashCode(), bullet2.hashCode());
+
+        HashSet<Bullet> hashSet = new HashSet<>();
+        hashSet.add(bullet1);
+        assertTrue(hashSet.contains(bullet1));
+        assertFalse(hashSet.contains(bullet3));
+    }
+
+    @Test
+    public void testGetters() {
+        assertEquals(0, Double.compare(bullet1.getAngle(), 3.1));
+        assertEquals(bullet1.getPlayerId(), 0);
+        assertEquals(bullet3.getPlayerId(), 4);
+        assertEquals(0, Double.compare(bullet1.getSpeed(), 3.3));
+    }
+}
